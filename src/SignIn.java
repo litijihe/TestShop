641a658a60ed846764ee71b3a5a615175ecd14e4
litijihe <3196992723@qq.com>
2020-12-26 17:27:11 +0800

    进行对比，并且显示登陆情况

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,3 +1,6 @@
+import sun.security.util.Password;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class SignIn {
@@ -8,7 +11,22 @@ public class SignIn {
         System.out.println("你输入的用户名为：" + UserName);
 
         System.out.println("请输入你的密码：");
-        String UserPassword=sc.next();
+        String UserPassword=sc.next();//阻塞函数next
         System.out.println("你输入的密码为：" + UserPassword);
+
+        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\TestShop\\src\\UserData.xlsx");
+        ReadExcel readExcel=new ReadExcel();
+        User user[]=readExcel.readExcel(file);
+        for (int i=0;i<user.length;i++)
+        {
+            if (UserName.equals(user[i].getUsername()) && UserPassword.equals(user[i].getPassword()))
+            {
+                System.out.println("登录成功！");
+                break;
+            }
+            else{
+                System.out.println("登陆失败！");
+            }
+        }
     }
 }

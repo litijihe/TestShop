7efe2a4beb64953c2096bd7006725b286de4f330
lenovo <jlsdgh>
2020-12-27 17:10:16 +0800

    添加商品列表和实体类bean

---
+++ src/Product.java
@@ -0,0 +1,40 @@
+public class Product {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    private String id;
+    private String name;
+    private float price;
+    private String desc;
+
+
+}
--- src/SignIn.java
+++ src/SignIn.java
@@ -1,15 +1,16 @@
-import sun.security.util.Password;
-
-import java.io.File;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class SignIn {
     public static void main(String[] args) throws ClassNotFoundException {
 
-        /*
-        alt + inter 导包
+        /*部分常用快捷键如下：
+         alt + inter 导包
          Ctrl + alt + L 使得代码自动变换为规范格式
+         Ctrl + O  查看我们继承的类或者接口中的方法，以及我们要实现的方法
+         Ctrl + 鼠标左键 进类中查看代码
+         Alt + Insert    set/get; 构造方法;  toString; 重写方法
+         Ctrl+Alt+T 将代码包在一个块中，例如try/catch  ;synchronized等
           */
         boolean bool = true;
         while (bool) {
@@ -22,19 +23,23 @@ public class SignIn {
             String UserPassword = sc.next();//阻塞函数next
             /*System.out.println("你输入的密码为：" + UserPassword);*/
 
-           // File file = new File("C:\\Users\\lenovo\\IdeaProjects\\TestShop\\src\\UserData.xlsx");
+            // File file = new File("C:\\Users\\lenovo\\IdeaProjects\\TestShop\\src\\UserData.xlsx");
 
-            InputStream in=Class.forName("SignIn").getResourceAsStream("/UserData.xlsx");
-            ReadExcel readExcel = new ReadExcel();
-            User user[] = readExcel.readExcel(in);
+            InputStream inuser = Class.forName("SignIn").getResourceAsStream("/UserData.xlsx");
+            InputStream inproduct= Class.forName("SignIn").getResourceAsStream("/ProductData.xlsx");
+
+            ReadUserExcel readExcel = new ReadUserExcel();
+            User user[] = readExcel.readExcel(inuser);
             for (int i = 0; i < user.length; i++) {
                 if (UserName.equals(user[i].getUsername()) && UserPassword.equals(user[i].getPassword())) {
-                    System.out.println("登录成功！");
+                    //System.out.println("登录成功！");
+
+
                     bool = false;
                     break;
                 } else {
                     if (i == (user.length - 1)) {
-                        System.out.println("登陆失败！");
+                        System.out.println("登陆失败！密码或者用户名错误，请重新输入！");
                     } else {
                         continue;
                     }

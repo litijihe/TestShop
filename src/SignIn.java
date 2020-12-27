dddf7e5052d0249b41e8ad1e6342e7812d7b0b71
lenovo <jlsdgh>
2020-12-27 16:00:44 +0800

    解决一个输入流问题

--- src/SignIn.java
+++ src/SignIn.java
@@ -5,11 +5,14 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class SignIn {
-        public static void main (String[]args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException {
 
-
-            boolean bool=true;
-            while (bool){
+        /*
+        alt + inter 导包
+         Ctrl + alt + L 使得代码自动变换为规范格式
+          */
+        boolean bool = true;
+        while (bool) {
             System.out.println("请输入你的名字：");
             Scanner sc = new Scanner(System.in);
             String UserName = sc.next();
@@ -19,15 +22,15 @@ public class SignIn {
             String UserPassword = sc.next();//阻塞函数next
             /*System.out.println("你输入的密码为：" + UserPassword);*/
 
-            File file = new File("C:\\Users\\lenovo\\IdeaProjects\\TestShop\\src\\UserData.xlsx");
+           // File file = new File("C:\\Users\\lenovo\\IdeaProjects\\TestShop\\src\\UserData.xlsx");
 
-            // InputStream in=Class.forName("SignIn").getResourceAsStream("/UserData.xlsx");
+            InputStream in=Class.forName("SignIn").getResourceAsStream("/UserData.xlsx");
             ReadExcel readExcel = new ReadExcel();
-            User user[] = readExcel.readExcel(file);
+            User user[] = readExcel.readExcel(in);
             for (int i = 0; i < user.length; i++) {
                 if (UserName.equals(user[i].getUsername()) && UserPassword.equals(user[i].getPassword())) {
                     System.out.println("登录成功！");
-                    bool=false;
+                    bool = false;
                     break;
                 } else {
                     if (i == (user.length - 1)) {
